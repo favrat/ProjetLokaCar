038b4d1d489028e4d9cfd0323d31cbde358ab175
Favrat <Alex.favrat@gmail.com>
2017-05-24 13:39:54 +0200

    ModeleDetailActivity

--- LokaCar/app/src/main/java/eni/fr/lokacar/ModeleDetailActivity.java
+++ LokaCar/app/src/main/java/eni/fr/lokacar/ModeleDetailActivity.java
@@ -3,6 +3,7 @@ package eni.fr.lokacar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,31 +11,33 @@ import android.widget.Toast;
 import java.util.List;
 
 import eni.fr.lokacar.dao.VehiculeDao;
+import eni.fr.lokacar.model.Marque;
 import eni.fr.lokacar.model.Vehicule;
 
 public class ModeleDetailActivity extends AppCompatActivity {
 
     private Vehicule vehicule;
+    private Marque marque;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modele_detail);
+
         Intent i = new Intent(ModeleDetailActivity.this, MainActivity.class);
         vehicule = getIntent().getParcelableExtra("vehicule");
+        marque = getIntent().getParcelableExtra("marque");
 
         TextView tvModeleDetail = (TextView) findViewById(R.id.et_modeleDetail);
         tvModeleDetail.setText(vehicule.getDesignation());
-        //TextView tvMarque = (TextView) findViewById(R.id.et_marque);
-        //tvMarque.setText(vehicule.getMarque());
+        //TextView tvMarque = (TextView) findViewById(R.id.et_marqueDetail);
+        // tvMarque.setText(marque.getLibelle());
         TextView tvImmat = (TextView) findViewById(R.id.et_immatDetail);
         tvImmat.setText(vehicule.getCodeNationalIdentificationType());
         TextView tvCouleur = (TextView) findViewById(R.id.et_couleurDetail);
         tvCouleur.setText(vehicule.getCouleur());
         TextView tvPrix = (TextView) findViewById(R.id.et_prixDetail);
         tvPrix.setText(vehicule.getPrix());
-        //i.putExtra("designation", String.valueOf(vehicule.getDesignation()));
-        /*VehiculeDao dao = new VehiculeDao(this);
-        final List<Vehicule> listeVehicule = dao.get();*/
+
         Toast.makeText(this, String.valueOf(vehicule.getDesignation()), Toast.LENGTH_SHORT).show();
     }
 }
